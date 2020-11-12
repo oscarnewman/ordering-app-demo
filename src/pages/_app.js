d2417898f5da4d0e4e03dc03e46fb083812be572
Oscar Newman <newman.oscar@gmail.com>
2020-11-11 21:13:53 -0500

    Add nProgress

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,24 @@
 import '@/styles/tailwind.css'
+import '@/styles/nprogress.css'
+
+import { useRouter } from 'next/router'
+import nProgress from 'nprogress'
 import { useEffect } from 'react'
 import smoothscroll from 'smoothscroll-polyfill'
+
 function MyApp({ Component, pageProps }) {
+	const router = useRouter()
+
+	useEffect(() => {
+		nProgress.configure({
+			showSpinner: false,
+		})
+
+		router.events.on('routeChangeStart', nProgress.start)
+		router.events.on('routeChangeComplete', nProgress.done)
+		router.events.on('routeChangeError', () => nProgress.done(true))
+	}, [])
+
 	useEffect(() => {
 		smoothscroll.polyfill()
 	}, [])

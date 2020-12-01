404ae10558d4f104ad7bee54e2d381283d109668
Oscar Newman <newman.oscar@gmail.com>
2020-12-01 12:45:50 -0600

    Animate tabs with famer motion

--- src/components/ui/Tabs/Tab.tsx
+++ src/components/ui/Tabs/Tab.tsx
@@ -1,26 +1,41 @@
 import { cx } from '@/util/classes'
+import { forwardRef, RefObject } from 'react'
 
 type Props = {
 	name: string
 	active?: boolean
+	animating: boolean
 	onClick: () => void
 }
 
-function Tab({ name, onClick, active = false }: Props) {
+function Tab(
+	{ name, onClick, active = false, animating }: Props,
+	ref: RefObject<any>
+) {
+	const showActiveState = active && !animating
 	return (
 		<button
+			ref={ref}
 			onClick={onClick}
 			className={cx(
-				'cursor-pointer whitespace-no-wrap px-2 py-2 rounded text-sm flex-shrink-0',
+				'cursor-pointer whitespace-no-wrap px-4 py-2 text-sm flex-shrink-0 focus:outline-none',
+				'border-b-4 font-medium',
 				{
-					'text-gray-600': !active,
-					'active border-b-2 border-theme-secondary text-white': active,
+					'text-gray-400': !active,
+					'text-theme-primary': active,
+					'border-transparent': !showActiveState,
+					'show-border': showActiveState,
 				}
 			)}
 		>
 			{name}
+			<style jsx>{`
+				.show-border {
+					border-width-bottom: var(--border-width);
+				}
+			`}</style>
 		</button>
 	)
 }
 
-export default Tab
+export default forwardRef(Tab)

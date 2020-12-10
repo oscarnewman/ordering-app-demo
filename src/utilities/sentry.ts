e4b838efc313cc5841ef52fc44775a9009f3d4bf
Oscar Newman <newman.oscar@gmail.com>
2020-12-10 16:44:58 -0600

    Cleanup env and destructure env variables

--- src/utilities/sentry.ts
+++ src/utilities/sentry.ts
@@ -1,13 +1,18 @@
 import { RewriteFrames } from '@sentry/integrations'
 import * as Sentry from '@sentry/node'
 
+const {
+	NEXT_PUBLIC_SENTRY_DSN,
+	NEXT_IS_SERVER,
+	NEXT_PUBLIC_SENTRY_SERVER_ROOT_DIR,
+	NEXT_PUBLIC_COMMIT_SHA,
+	NODE_ENV,
+} = process.env
+
 export const initSentry = () => {
-	if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
+	if (NEXT_PUBLIC_SENTRY_DSN) {
 		const integrations = []
-		if (
-			process.env.NEXT_IS_SERVER === 'true' &&
-			process.env.NEXT_PUBLIC_SENTRY_SERVER_ROOT_DIR
-		) {
+		if (NEXT_IS_SERVER === 'true' && NEXT_PUBLIC_SENTRY_SERVER_ROOT_DIR) {
 			// For Node.js, rewrite Error.stack to use relative paths, so that source
 			// maps starting with ~/_next map to files in Error.stack with path
 			// app:///_next
@@ -15,7 +20,7 @@ export const initSentry = () => {
 				new RewriteFrames({
 					iteratee: frame => {
 						frame.filename = frame.filename.replace(
-							process.env.NEXT_PUBLIC_SENTRY_SERVER_ROOT_DIR,
+							NEXT_PUBLIC_SENTRY_SERVER_ROOT_DIR,
 							'app:///'
 						)
 						frame.filename = frame.filename.replace('.next', '_next')
@@ -26,10 +31,10 @@ export const initSentry = () => {
 		}
 
 		Sentry.init({
-			enabled: process.env.NODE_ENV === 'production',
+			enabled: NODE_ENV === 'production',
 			integrations,
-			dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-			release: process.env.NEXT_PUBLIC_COMMIT_SHA,
+			dsn: NEXT_PUBLIC_SENTRY_DSN,
+			release: NEXT_PUBLIC_COMMIT_SHA,
 		})
 	}
 }

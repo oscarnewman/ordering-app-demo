55665a841ed6d2ef4fb0d7cdfab1235024a86723
Oscar Newman <newman.oscar@gmail.com>
2020-12-18 23:06:42 -0600

    Cleanup modifier sets

--- src/components/ModifierSetSelection/ModifierRow.tsx
+++ src/components/ModifierSetSelection/ModifierRow.tsx
@@ -4,26 +4,46 @@ import classNames from 'classnames'
 import { PlusIcon } from '../ui/icons'
 
 interface Props {
+	/** The modifier object */
 	modifier: Modifier
+
+	/** Whehter this row is selected */
 	selected?: boolean
+
+	/** Callback when this row is clicked */
 	onClick: (string) => void
 }
+
+/**
+ * A single row representing one modifier of a ModifierSet
+ */
 const ModifierRow = ({ modifier, selected = false, onClick }: Props) => {
 	return (
 		<button
-			className="py-2 flex justify-between items-center focus:outline-none"
+			className="py-3 flex justify-between items-center focus:outline-none"
 			onClick={() => onClick(modifier.id)}
 		>
 			<div className="flex items-center">
-				<div
-					className={classNames('w-5 h-5 rounded-full border-4 mr-2', {
-						'bg-gray-400 border-white': !selected,
-						'bg-indigo-600 border-indigo-200': selected,
-					})}
-				/>
+				<div className="relative mr-4">
+					<div
+						className={classNames('w-4 h-4 rounded-full border z-10', {
+							'bg-gray-200 border-gray-200': !selected,
+							'bg-theme-secondary border-theme-secondary': selected,
+						})}
+					/>
+					<div
+						className={classNames(
+							'absolute w-6 h-6 -top-1 -left-1 z-0 rounded-full transition-transform transform duration-150',
+							{
+								'scale-75': !selected,
+								'bg-theme-secondary opacity-25 scale-100': selected,
+							}
+						)}
+					/>
+				</div>
 				<p
 					className={classNames({
-						'text-indigo-800': selected,
+						'text-theme-primary': selected,
 						'text-gray-500': !selected,
 					})}
 				>
--- src/components/ModifierSetSelection/index.tsx
+++ src/components/ModifierSetSelection/index.tsx
@@ -1,52 +1,62 @@
-import { ModifierSet, Modifier } from '@/types/Menu'
+import { ModifierSet } from '@/types/Menu'
 import { useCallback, useEffect, useState } from 'react'
 import Stack from '../ui/Stack'
 import ModifierRow from './ModifierRow'
 
 interface Props {
+	/** The modifier set to display */
 	modifierSet: ModifierSet
+
+	/** Callback when a new set of modifiers are selected */
 	onSelect: (modifierIds: string[]) => void
 }
 
+/**
+ * A radio-style group to display a modifier set on an item
+ */
 const ModifierSetSelection = ({ modifierSet, onSelect }: Props) => {
 	const [selected, setSelected] = useState(modifierSet.preselectedModifiers)
+	const isSelected = useCallback(id => selected.includes(id), [selected])
 
 	const handleClick = modifierId => {
+		// This works like a regular single-select radio group
 		if (modifierSet.max === 1 && modifierSet.min === 1) {
 			setSelected([modifierId])
-		} else if (
-			selected.includes(modifierId) &&
-			selected.length > modifierSet.min
-		) {
-			// remove
+		}
+
+		// Removing a modifier
+		else if (isSelected(modifierId) && selected.length > modifierSet.min) {
 			setSelected(selected.filter(id => id !== modifierId))
-		} else if (
-			!selected.includes(modifierId) &&
+		}
+
+		// Adding a new modifier
+		else if (
+			!isSelected(modifierId) &&
 			(!modifierSet.max || selected.length < modifierSet.max)
 		) {
-			// add
 			setSelected([...selected, modifierId])
 		}
 	}
 
+	// Trigger onSelect callback
 	useEffect(() => {
 		onSelect(selected)
-	}, [selected])
-
-	const isSelected = useCallback(id => selected.includes(id), [selected])
+	}, [onSelect, selected])
 
 	return (
 		<Stack space={4}>
-			<h3 className="font-bold">{modifierSet.name}</h3>
-			<Stack divider>
-				{modifierSet.modifiers.map(modifier => (
-					<ModifierRow
-						key={modifier.id}
-						modifier={modifier}
-						onClick={handleClick}
-						selected={isSelected(modifier.id)}
-					/>
-				))}
+			<Stack space={1}>
+				<h3 className="font-bold">{modifierSet.name}</h3>
+				<Stack divider>
+					{modifierSet.modifiers.map(modifier => (
+						<ModifierRow
+							key={modifier.id}
+							modifier={modifier}
+							onClick={handleClick}
+							selected={isSelected(modifier.id)}
+						/>
+					))}
+				</Stack>
 			</Stack>
 		</Stack>
 	)

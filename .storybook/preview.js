1898bae0f924f6f8b9501be0d84722523bcc2931
Oscar Newman <newman.oscar@gmail.com>
2020-12-19 18:37:06 -0600

    Add toast

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,40 +6,51 @@ import smoothscroll from 'smoothscroll-polyfill'
 import { withNextRouter } from 'storybook-addon-next-router'
 import { withThemes } from 'storybook-addon-themes/react'
 import Layout from './Layout'
+import Providers from '@/components/Providers'
 
 // Injext `Image` component mock into our storybook component
 // which is just a regular img tag.
 // Workaround until https://www.npmjs.com/package/@next/plugin-storybook
 // is released
 Object.defineProperty(nextImage, 'default', {
-  configurable: true,
-  value: (props) => {
-    return <img {...props} />;
-  },
-});
+	configurable: true,
+	value: props => {
+		return <img {...props} />
+	},
+})
 
-function ThemeDecorator({children, themeName}) {
+function ThemeDecorator({ children, themeName }) {
 	useEffect(() => {
 		smoothscroll.polyfill()
 	}, [])
-	let theme = {'Sharkys': SharkysTheme}[themeName]
+	let theme = { Sharkys: SharkysTheme }[themeName]
 	if (!theme) theme = MarbleTheme
-	return <LocationSettingsProvider settings={theme}>{children}</LocationSettingsProvider>
+	return (
+		<LocationSettingsProvider settings={theme}>
+			{children}
+		</LocationSettingsProvider>
+	)
 }
 
 export const parameters = {
-	actions: { argTypesRegex: "^on[A-Z].*" },
+	actions: { argTypesRegex: '^on[A-Z].*' },
 	themes: {
 		Decorator: ThemeDecorator,
 		list: [
-			{name: 'Marble', color: '#87EEBF'},
-			{name: 'Sharkys', color: '#F07321', default: true}
-		]
-	}
+			{ name: 'Marble', color: '#87EEBF' },
+			{ name: 'Sharkys', color: '#F07321', default: true },
+		],
+	},
 }
 
-export const decorators  = [
+export const decorators = [
 	withNextRouter(),
 	withThemes,
-	Story => <Layout><Story /></Layout>,
+	Story => (
+		<Providers>
+			<Layout>
+				<Story />
+			</Layout>
+		</Providers>
+	),
 ]
